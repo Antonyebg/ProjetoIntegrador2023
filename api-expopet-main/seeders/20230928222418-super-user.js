47777ea61b805f2a7265203d23adf4e1aa10709e
antony <antonyebg@hotmail.com>
2023-10-05 20:24:21 -0300

    seeder usuario admin

---
+++ api-expopet-main/seeders/20230928222418-super-user.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [{
+      nome: 'Admin',
+      email: 'admin@admin.com',
+      // admin
+      senha: '$2a$10$jCMd5OFd5YvImlcxZMRr0eLAP4lnb5qUxCCkPdf7k5kvFZy7Esl9a',
+      telefone: '00000000000',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
